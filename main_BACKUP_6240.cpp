43f19e792e2fcd26dd1b9c70af6fb7287e3e930c
PeterHiroshi <peter8icestone@gmail.com>
2018-07-10 15:13:10 +0800

    conflict resolve

---
+++ main_BACKUP_6240.cpp
@@ -0,0 +1,26 @@
+#include <QApplication>
+#include <QDialog>
+#include <QInputDialog>
+#include <QLabel>
+#include <QTextCodec>
+#include <QGridLayout>
+
+#include "mainwindow.h"
+
+int main(int argc, char *argv[]){
+
+<<<<<<< HEAD
+    // remote develop branch
+=======
+    // remote develop
+>>>>>>> c6fe3777bc45989c0b6657669d44fee7e90b5b81
+
+    QApplication a(argc, argv);
+
+    MainWindow mainWin;
+
+    mainWin.showDialog();
+
+    return a.exec();
+
+}
