675fc201373d3649b4ac68f314b35d03bcf06e2a
PeterHiroshi <peter8icestone@gmail.com>
2018-07-24 19:37:19 +0800

    latest version okey!!!

--- mainwindow.h
+++ mainwindow.h
@@ -53,7 +53,7 @@ private:
     // layer number of version 64
     const int mode64_layNum = 8;
     // layer number of version 100
-    const int mode100_layNum = 5;
+    const int mode100_layNum = 8;
 
 public slots:
     virtual void accept();
